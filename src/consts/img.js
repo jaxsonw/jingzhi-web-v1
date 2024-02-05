869e39b04bdbd501a447a3979867baa9cdebf82d
Xiaolei Cheng <xiaolei.cheng@tripalink.com>
2024-02-05 17:18:09 +0800

    feat: update icon

---
+++ src/consts/img.js
@@ -0,0 +1,30 @@
+// 白色带icon logo
+export const icon_logo_white = 'https://ew6.cn/agicto_logo_white.png'
+// 彩色带icon logo
+export const icon_logo_color = 'https://ew6.cn/icon_logo_color.png'
+// login 汇集精彩
+export const login_huijujingcai = 'https://ew6.cn/login_huijijingcai.png'
+
+// 360彩色icon
+export const sanliuling_icon_color = 'https://ew6.cn/360_icon_color.png'
+
+// agicto彩色icon
+export const agicto_icon_color = 'https://ew6.cn/agicto_icon_color.png'
+
+// gpt彩色icon
+export const gpt_icon_color = 'https://ew6.cn/gpt_icon_color.png'
+
+// 百川彩色icon
+export const baichuan_icon_color = 'https://ew6.cn/baichuan_icon_color.png'
+
+// 浑源彩色icon
+export const hunyuan_icon_color = 'https://ew6.cn/hunyuan_icon_color.png'
+
+// 通义彩色icon
+export const tongyi_icon_color = 'https://ew6.cn/tongyi_icon_color.png'
+
+// 文心彩色icon
+export const wenxin_icon_color = 'https://ew6.cn/wenxin_icon_color.png'
+
+// LLMA2彩色icon
+export const llma_icon_color = 'https://ew6.cn/llma_icon_color.png'
