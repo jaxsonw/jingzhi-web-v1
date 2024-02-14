a2dd7b09562f8e695b441152dcdba89ab768c556
Xiaolei Cheng <xiaolei.cheng@tripalink.com>
2024-02-15 00:29:51 +0800

    feat: update script

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,19 +18,22 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Script src="https://res.wx.qq.com/open/js/jweixin-1.6.0.js" />
+      <Script id="weixinopenjs" src="https://res.wx.qq.com/open/js/jweixin-1.6.0.js" />
       <Script
-        dangerouslySetInnerHTML={{
-          __html: ` 
-        var _hmt = _hmt || [];
+        id="baidutongji"
+        //   dangerouslySetInnerHTML={{
+        //     __html: `
+        //  `
+        //   }}
+      >
+        {`var _hmt = _hmt || [];
         (function() {
           var hm = document.createElement("script");
           hm.src = "https://hm.baidu.com/hm.js?983fca994414ee989e1b88bd0193f557";
           var s = document.getElementsByTagName("script")[0]; 
           s.parentNode.insertBefore(hm, s);
-        })();`
-        }}
-      />
+        })();`}
+      </Script>
       <body className={inter.className}>
         <ToastContainer
           style={{
