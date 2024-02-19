6b197a54c36e88843778da52f923bc78dd2d5051
Xiaolei Cheng <xiaolei.cheng@tripalink.com>
2024-02-19 08:39:36 +0800

    feat: update

--- src/app/space/costs/page.jsx
+++ src/app/space/costs/page.jsx
@@ -1,24 +1,30 @@
 'use client'
 import { useEffect, useState } from 'react'
 import moment from 'moment'
-import { AreaChart, BarChart, Card, Title, DatePicker,Select, SelectItem } from '@tremor/react'
-  import { zhCN } from 'date-fns/locale'
+import { AreaChart, BarChart, Card, Title, DatePicker, Select, SelectItem } from '@tremor/react'
+import { zhCN } from 'date-fns/locale'
 import { consumeChart, consumeModelList, getModelPriceList } from '../../../services/overflow'
 
 export default function Overflow() {
     const [singleDayDate, setSingleDayDate] = useState(moment().format('YYYY-MM-DD'))
     const [dataList, setDataList] = useState([])
-    const [modelList,setModelList ] = useState([])
+    const [modelList, setModelList] = useState([])
     const [totalFee, setTotalFee] = useState(0)
 
+    const [selectModel,setSelectModel] = useState(null)
+
+    console.log("selectModel", modelList)
+ 
     const init = async () => {
-        const modelListRes =await getModelPriceList()
-        setModelList(modelListRes?.data?.modelList)
-       
+        const modelListRes = await getModelPriceList()
+        setModelList(modelListRes?.data?.modelPriceList)
+
     }
 
     const getSingleDayData = async () => {
-        const consumeModelRes = await consumeModelList()
+        const consumeModelRes = await consumeModelList({
+            date: singleDayDate
+        })
 
         const consumeModel = consumeModelRes?.data?.modelList?.map(item => ({
             ...item,
@@ -29,13 +35,14 @@ export default function Overflow() {
     }
 
     useEffect(() => {
-        getSingleDayData()
-    }, [singleDayDate])
+        init()
+    }, [])
+
     useEffect(() => {
         getSingleDayData()
     }, [singleDayDate])
 
-     return (
+    return (
         <>
             <div className="flex justify-between items-center mb-4 mt-[18px]">
                 <span className="text-[#545759]">单日消耗统计：<span className="text-[#3162FF] font-bold text-xl">{totalFee?.toFixed(4)}元</span> </span>
@@ -46,59 +53,54 @@ export default function Overflow() {
                     onValueChange={e => setSingleDayDate(moment(e)?.format('YYYY-M-D'))}
                     className="max-w-sm"
                 />
-            </div> 
+            </div>
             <Card>
-            <BarChart
-                className="mt-6"
-                data={dataList}
-                index="model"
-                categories={['消费金额']}
-                colors={['blue-600']}
-                valueFormatter={v => Number(v)?.toFixed(4)}
-                yAxisWidth={80}
-                showXAxis
-            />
-        </Card>
+                <BarChart
+                    className="mt-6"
+                    data={dataList}
+                    index="model"
+                    categories={['消费金额']}
+                    colors={['blue-600']}
+                    valueFormatter={v => Number(v)?.toFixed(4)}
+                    yAxisWidth={80}
+                    showXAxis
+                />
+            </Card>
 
 
-        <div className="flex justify-between items-center mb-4 mt-[54px]">
+            <div className="flex justify-between items-center mb-4 mt-[54px]">
                 <span className="text-[#545759]">所选日期范围共消耗： <span className="text-[#3162FF] font-bold text-xl">{totalFee?.toFixed(4)}元</span> </span>
                 <div className="flex items-center justify-end">
-                     <Select className="mr-4">
-                         <SelectItem value="1" >
-                             Kilometers
-                         </SelectItem>
-                         <SelectItem value="2" >
-                             Meters
-                         </SelectItem>
-                         <SelectItem value="3" >
-                             Miles
-                         </SelectItem>
-                         <SelectItem value="4" >
-                             Nautical Miles
-                         </SelectItem>
-                     </Select>
-                <DatePicker
-                    locale={zhCN}
-                    placeholder="请选择日期"
-                    value={new Date(singleDayDate)}
-                    onValueChange={e => setSingleDayDate(moment(e)?.format('YYYY-M-D'))}
-                    className="max-w-sm"
-                />
+                    <div className="mr-6">
+                        <Select onValueChange={(e)=>console.log("e",e)} placeholder="选择模型" >
+                            {
+                                modelList?.map((item, index) => <SelectItem key={index} value={item?.model} >
+                                    {item?.model}
+                                </SelectItem>)
+                            } 
+                        </Select>
+                    </div>
+                    <DatePicker
+                        locale={zhCN}
+                        placeholder="请选择日期"
+                        value={new Date(singleDayDate)}
+                        onValueChange={e => setSingleDayDate(moment(e)?.format('YYYY-M-D'))}
+                        className="max-w-sm"
+                    />
                 </div>
-            </div> 
+            </div>
             <Card>
-            <BarChart
-                className="mt-6"
-                data={dataList}
-                index="model"
-                categories={['消费金额']}
-                colors={['blue-600']}
-                valueFormatter={v => Number(v)?.toFixed(4)}
-                yAxisWidth={80}
-                showXAxis
-            />
-        </Card>
+                <BarChart
+                    className="mt-6"
+                    data={dataList}
+                    index="model"
+                    categories={['消费金额']}
+                    colors={['blue-600']}
+                    valueFormatter={v => Number(v)?.toFixed(4)}
+                    yAxisWidth={80}
+                    showXAxis
+                />
+            </Card>
         </>
     )
 }
