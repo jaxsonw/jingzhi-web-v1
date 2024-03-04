d49db0175f003cf5f87ceeb9946127fbb66af085
Xiaolei Cheng <xiaolei.cheng@tripalink.com>
2024-03-04 23:03:00 +0800

    feat: update

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -12,8 +12,8 @@ const hiddenPath = ['/space', "/login"]
 
 const navigation = [
   { name: '首页', href: '/' },
-  { name: '开发文档', href: '/docs', target: '__blank' },
-  { name: '计费规则', href: '/docs/pricing', target: '__blank' }
+  { name: '开发文档', href: '/docs', target: '_self' },
+  { name: '计费规则', href: '/docs/pricing', target: '_self' }
 ]
 
 const Header = () => {
