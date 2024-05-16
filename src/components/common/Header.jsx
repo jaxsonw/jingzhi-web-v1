0a455be927e462a34b57b8c29ddf8cdc2dcb9f9c
韩吉鑫 <764671440@qq.com>
2024-05-16 10:25:15 +0800

    更新文件 Header.jsx

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -13,7 +13,7 @@ const hiddenPath = ['/space', '/login', '/code']
 const navigation = [
   { name: '首页', href: '/' },
   { name: '模型广场', href: '/model', target: '_self' },
-  { name: '模型排行榜', href: '/llm-leader', target: '_self' },
+  { name: '模型排行榜', href: '/llm-leaderboard', target: '_self' },
   { name: '调试平台', href: '/playground', target: '_self' },
   { name: 'Prompt工程', href: '/prompt', target: '_self' },
   { name: '开发文档', href: 'https://docs.agicto.com', target: '_blank' },
