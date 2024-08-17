173bb07663c5146a28c3035d6c25d0f51a456956
Xiaolei <xiaolei.cheng@tripalink.com>
2024-08-17 23:35:08 +0800

    feat: update model detail page

--- src/app/model/[modelName]/content.jsx
+++ src/app/model/[modelName]/content.jsx
@@ -67,12 +67,12 @@ main();`
                 <img className="w-[10px] h-[14px] mr-[4px]" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAcCAYAAABh2p9gAAACC0lEQVRIS62WvU7bUBSAz73XJrabOA1UhAxUNDRLt1ZN2foWeQcYujAwAlHHUjWo7QaiUtWtQ4r6AB2glSqVBbGgVFilYgQSHP/c3JtUrhMnim4SG9mz9fn8fOccI7jdg9Pa4uMOyPsyUXOUXW/d2GdrHgpF55VIUjl6jrH8SZaSOQQIKKvXGtZp4RbAEplWj5cY6nz2YRh4m3KXXZYt5/xlVCDW1UdFhDsfJaIVfJjLW6yxYzrGCgB0IgF1dWEJkFKVpTtzPsyhjDffZG28UYOa2ytdqBomEvOLipT6LhNt1is7b7uUcbOSymTKFxe/rME+TAQqyv28QrQDiSRzPoxyxpuvZmxWNsBwhps6DojTWv4JIOWrTLSsD3MY5fX3TfvPKgC0RYaMAJaIrp48BdSuylJyzlPDg7V4/Z3pw/43ICTQV4Nj9KHfTcopv3rbjWwkTNTlUWrsmo6xPC4yYZd19cEzQIkvk9QYN11BDQVqcMbNrbSdLf+FH3bYEQ2Aulb4OSWliz01Wux6756DXojUCBVh7MBuyod951wv5deiaQgVofeSruWLAFP7Q02pZG28PjivoYEAEK82/pfjFbubTayjF1QozuXQL3uc6yugihdsczuVubsZecH2qOITYFVm7PkNA74Fi3bixh5wLpRSUYCBUnGd0UApwaH/3bBOH0Y6o0PjNvJX5B/ruW4syLef1AAAAABJRU5ErkJggg==" />
                 <span>返回</span>
             </div>
-            <Link href="/docs">
+            {/* <Link href="/docs">
                 <div className="flex items-center">
                     <img className="w-[12px] h-[14px] " src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAcCAYAAAB75n/uAAAAAXNSR0IArs4c6QAAAhhJREFUSEu11snLzVEcx/HXg5AyLWSjCFGysGFrY4qFEhY2LAwLMpQiREQRZSgrGywsKGWjiH9AyFyEpRILQ+Ex9+HcerrufZzr5tTtV797zvf9+46f0+P3GoJDWINh5V3N4zbW4Wa7zT3ljyW4gAd4iR8V1idgCu5idTtIAPntwhbMwtMK49myCYcxCA+xCreaz8b4AOzBSszDkwpAzm3DRlzGMjwrkHt9z/cF5AsCeNwBYH05Mw7n8Rp5d7VhoxvAdmzAXDzCIpzDC2zGlUD+FZCzK3AWZ3AHAzEfC0qYl+J+N4CxxfjMEo5UXuyl5FPqO1L63QBiN4ZmYHQp7W8Yj4M4gX3dAlrVwyRcL97t/R+AqaWKTqMtIL2RpluIuN3fSnLTCzfwHVWAJGo/tlb0RLYcKdOgtxaQ0I3BxErAc7wqia7yIIARGFkJeIt3nQAGYzd2VgIOpCTxuTZESdwcLK5M8iVcK3urQtT48FEV2pBwvunjaRUgVZR5n3nzN/EJIEPuOKqrKCKyvEhoTR+cKuP6a20O4nESPbyNB/Eq4MaA+4gvJUxVIUqSZyMjN93ZLFLvi4b/IZG1HgwtJRoZbc5BYp5YH8PJFmVc5UHOJTzT2njwqYhKns2rLaAT0e+v/3KViSZH6X5N03+9trSDTC6T9WgGZjcXr1aA2IuUTi9XmYsNQBorMre2w6tjK8gHpC9yb+r9CYTzsEFYol9MAAAAAElFTkSuQmCC" />
                     <span className="ml-[4px]">帮助文档</span>
                 </div>
-            </Link>
+            </Link> */}
         </div>
         <div className="flex justify-between">
             <div className="flex flex-col">
