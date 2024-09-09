9d035cd8373a6f2d06897a2740b8db7cd25f30d4
飘丨雪 <2945414535@qq.com>
2024-09-09 15:21:51 +0800

    fix:baseurl获取修改

--- src/consts/env.js
+++ src/consts/env.js
@@ -1,3 +1,3 @@
-export const BASE_URL = typeof window !== 'undefined' ? process.env.NEXT_PUBLIC_API_BASE_URL : undefined
-console.log('BASE_URL', BASE_URL)
-// export const BASE_URL = 'http://47.93.21.166:12346/mapi'
+// export const BASE_URL = typeof window !== 'undefined' ? process.env.NEXT_PUBLIC_API_BASE_URL : undefined
+// console.log('BASE_URL', BASE_URL)
+export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
